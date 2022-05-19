7013c301b19c05ee5d176f9257e86f612c239fac
quietjun <105872347+yooongjun@users.noreply.github.com>
2022-05-20 01:42:26 +0900

    Update Testclass.java

--- GithubPractice/src/Package/Testclass.java
+++ GithubPractice/src/Package/Testclass.java
@@ -4,6 +4,8 @@ public class Testclass {
 
 	public static void main(String[] args) {
 		System.out.println("Github upload practice! ");
+		System.out.println("Github upload practice! ");
+		
 	}
 
 }
